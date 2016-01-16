1a14677403321a3b02de2c0033418ba8ff48038f
Seena Rowhani <srowhani@ciena.com>
2016-01-16 18:18:04 -0500

    database is up, and scrape job working

--- bin/twitterfy.js
+++ bin/twitterfy.js
@@ -13,7 +13,6 @@
     access_token_key: process.env.TWITTER_ACCESS_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_SECRET,
   });
-
   db.serialize(function(){
     [
       {
@@ -29,12 +28,12 @@
         params: [
           'id INTEGER PRIMARY KEY',
           'screen_name TEXT',
-          'image_url TEXT',
+          'profile_image TEXT',
           'location TEXT'
         ]
       }
     ].forEach(function(e){
-      db.run('create table if not exists ' + e.name + '( ' + e.params.join(', ') + ')')
+      db.run('create table if not exists ' + e.name + '( ' + e.params.join(', ') + ');')
     });
 
     client.stream('statuses/filter', {
@@ -42,7 +41,10 @@
       track: process.argv[2] || "hello"
     }, function(stream) {
       stream.on('data', function(tweet) {
-        console.log(tweet);
+        db.run('insert into tweets values(?, ?, ?)',
+          tweet.id, tweet.text, tweet.user.id);
+        db.run('insert into users values(?, ?, ?, ?)',
+          tweet.user.id, tweet.user.screen_name, tweet.user.profile_image_url, tweet.user.time_zone);
       });
       stream.on('error', function(error) {
         console.log(error);
