017b05a5ee6d2dc7fae0b34e6cdb4b47a8a0733d
Seena Rowhani <srowhani@ciena.com>
2016-01-17 11:50:00 -0500

    added sigint handler, only add tweets that have user location on, TODO restrict to US

--- bin/twitterfy.js
+++ bin/twitterfy.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
+
 /**
  * [description]
  * @param  {[type]} function( [description]
  * @return {[type]}           [description]
  */
-(function(Twitter, sqlite3) {
+(function(Twitter, sqlite3, chalk) {
   'use strict';
   var db = new sqlite3.Database('data/twitterfy.db');
   var client = new Twitter({
@@ -13,26 +14,14 @@
     access_token_key: process.env.TWITTER_ACCESS_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_SECRET,
   });
-  db.serialize(function(){
-    [
-      {
-        name: 'tweets',
-        params: [
-          'id INTEGER PRIMARY KEY',
-          'text TEXT',
-          'user_id TEXT'
-        ]
-      },
-      {
-        name: 'users',
-        params: [
-          'id INTEGER PRIMARY KEY',
-          'screen_name TEXT',
-          'profile_image TEXT',
-          'location TEXT'
-        ]
-      }
-    ].forEach(function(e){
+  db.serialize(function() {
+    [{
+      name: 'tweets',
+      params: ['id INTEGER PRIMARY KEY', 'text TEXT', 'user_id INTEGER']
+    }, {
+      name: 'users',
+      params: ['id INTEGER PRIMARY KEY', 'screen_name TEXT', 'profile_image TEXT', 'location TEXT']
+    }].forEach(function(e) {
       db.run('create table if not exists ' + e.name + '( ' + e.params.join(', ') + ');')
     });
 
@@ -40,15 +29,37 @@
       lang: 'en',
       track: process.argv[2] || "hello"
     }, function(stream) {
+      console.log(chalk.green('Running job...\nPress CTRL + C to exit.'));
       stream.on('data', function(tweet) {
-        db.run('insert into tweets values(?, ?, ?)',
-          tweet.id, tweet.text, tweet.user.id);
-        db.run('insert into users values(?, ?, ?, ?)',
-          tweet.user.id, tweet.user.screen_name, tweet.user.profile_image_url, tweet.user.time_zone);
+        if (tweet.user.location) {
+          db.run('insert into tweets values(?, ?, ?)',
+            tweet.id,
+            tweet.text,
+            tweet.user.id,
+            function(e, r) {
+              if(e) console.log(chalk.red(e));
+            }
+          );
+          db.run('insert into users values(?, ?, ?, ?)',
+            tweet.user.id,
+            tweet.user.screen_name,
+            tweet.user.profile_image_url,
+            tweet.user.location,
+            function(e, r) {
+              if(e)
+                if(e !== 'Error: SQLITE_CONSTRAINT: UNIQUE constraint failed: users.id')
+                  console.log(chalk.red(e));
+            }
+          );
+        }
       });
-      stream.on('error', function(error) {
-        console.log(error);
+      stream.on('error', function(e) {
+        if(e) console.log(chalk.bold.red(e));
       });
     });
   });
-})(require('twitter'), require('sqlite3'));
+})(require('twitter'), require('sqlite3'), require('chalk'));
+require('process').on('SIGINT', function(){
+  console.log('\nGoodbye!');
+  process.exit();
+});
