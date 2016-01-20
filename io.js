27c11dd6fc97cd194079dc086ebcc955eab2213f
Seena Rowhani <srowhani@ciena.com>
2016-01-20 15:24:05 -0500

    added ipc via sockets, clean code

--- io.js
+++ io.js
@@ -9,14 +9,17 @@ module.exports = function(server, db) {
     "use strict";
     var SELECT = 'select screen_name, text, location, user_id from tweets join users on tweets.user_id = users.id;';
     var io = require('socket.io')(server);
-    io.sockets.on('connection', function(socket) {
-        socket.on('ready', function(message) {
-            db.serialize(function(){
-              db.each(SELECT, function(err, row){
-                if(err) throw err;
-                io.sockets.emit('data', row);
-              });
-            });
+    var write = function(){
+      db.serialize(function(){
+          db.each(SELECT, function(err, row){
+            if(err) throw err;
+            io.sockets.emit('data', row);
+          });
         });
+    };
+    io.sockets.on('connection', function(socket) {
+        console.log(socket.id + " has connected...");
+        socket.on('ready', write);
+        socket.on('push', write);
     });
 }
