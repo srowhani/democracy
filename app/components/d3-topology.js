108f169d326bb1ad4ee52f0d9b1529e0646e6f91
Seena Rowhani <srowhani@ciena.com>
2016-01-20 11:43:35 -0500

    d3 just the tip

--- app/components/d3-topology.js
+++ app/components/d3-topology.js
@@ -25,12 +25,32 @@ export default Ember.Component.extend({
           obj[e.location] = +1;
       }
       Object.keys(obj).forEach(function(e){
-        document.querySelector('.' + e).style.fill = obj[e] > -1 ? `rgb(113, 115, ${219 + obj[e] * 2})` : `rgb(${201 - obj[e] * 2}, 58, 58)`;
+        if(obj[e] === 0)
+          document.querySelector('.' + e).style.fill = '';
+        else
+          document.querySelector('.' + e).style.fill = obj[e] > -1 ? `rgb(113, 115, ${219 + obj[e] * 2})` : `rgb(${201 - obj[e] * 2}, 58, 58)`;
       });
     })
     var svg = d3.select(this.element)
       .attr('width', 900)
       .attr('height', 550);
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(function(d) {
+        var state = d.properties.STATE_ABBR;
+        var vote;
+        if(!obj[state])
+          vote = 'Neutral';
+        else if(obj[state] > 0)
+          vote = 'Democratic';
+        else
+          vote = 'Republican';
+
+        return `<strong>State: </strong> ${state}<br/>`
+          + `<strong>Majority: </strong> ${vote}`;
+    })
+    svg.call(tip);
 
     var path = d3.geo.path().projection(d3.geo.albersUsa());
 
@@ -44,8 +64,13 @@ export default Ember.Component.extend({
           return 'states ' + d.properties.STATE_ABBR;
         })
         .attr('d', path)
-        .attr('fill', 'gray')
-        .attr('opacity', '0.8');
+        .on('mouseover', function(d){
+          tip.attr('class', 'd3-tip animate').show(d);
+        })
+        .on('mouseout', function(d){
+          tip.attr('class', 'd3-tip').show(d)
+          tip.hide();
+        });
       socket.emit('ready', {});
     });
   }
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
-
+  app.import('bower_components/d3-tip/index.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
