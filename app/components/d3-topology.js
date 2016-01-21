1a43c2c69c67f489464928f4cfdee8fb890604d8
Seena Rowhani <srowhani@ciena.com>
2016-01-21 13:36:04 -0500

    whateer

--- app/components/d3-topology.js
+++ app/components/d3-topology.js
@@ -6,29 +6,30 @@ export default Ember.Component.extend({
     let obj = {};
     let republican = 'votetrump,votefortrump,trump2016'.split(',');
     let democrat = 'votehillary,hillary2016,voteclinton,clinton2016,voteforclinton,votesanders,voteforsanders,votebernie,voteforbernie'.split(',');
-    socket.on('data', function(e){
+    socket.on('data', function(e) {
       let text = e.text.toLowerCase();
-      if(republican.some(function(e){
-        return text.includes(e);
-      })){
-        if(obj[e.location] !== undefined)
-          obj[e.location]--;
-        else
-          obj[e.location] = -1;
+      if (republican.some(function(e) {
+          return text.includes(e);
+        })) {
+        obj[e.location] = obj[e.location] || {
+          r: 0,
+          d: 0
+        };
+        obj[e.location]['r']++;
+      } else if (democrat.some(function(e) {
+          return text.includes(e);
+        })) {
+        obj[e.location] = obj[e.location] || {
+          r: 0,
+          d: 0
+        };
+        obj[e.location]['d']++;
       }
-      else if(democrat.some(function(e){
-        return text.includes(e);
-      })){
-        if(obj[e.location] !== undefined)
-          obj[e.location]++;
-        else
-          obj[e.location] = +1;
-      }
-      Object.keys(obj).forEach(function(e){
-        if(obj[e] === 0)
+      Object.keys(obj).forEach(function(e) {
+        if (obj[e].r - obj[e].d === 0)
           document.querySelector('.' + e).style.fill = '';
         else
-          document.querySelector('.' + e).style.fill = obj[e] > -1 ? `rgb(113, 115, ${219 + obj[e] * 2})` : `rgb(${201 - obj[e] * 2}, 58, 58)`;
+          document.querySelector('.' + e).style.fill = obj[e].r - obj[e].d > -1 ? `rgb(113, 115, ${219 + (obj[e].d - obj[e].r) * 2})` : `rgb(${201 + (obj[e].r - obj[e].d) * 2}, 58, 58)`;
       });
     })
     var svg = d3.select(this.element)
@@ -36,20 +37,7 @@ export default Ember.Component.extend({
       .attr('height', 550);
     var tip = d3.tip()
       .attr('class', 'd3-tip')
-      .offset([-10, 0])
-      .html(function(d) {
-        var state = d.properties.STATE_ABBR;
-        var vote;
-        if(!obj[state])
-          vote = 'Neutral';
-        else if(obj[state] > 0)
-          vote = 'Democratic';
-        else
-          vote = 'Republican';
-
-        return `<strong>State: </strong> ${state}<br/>`
-          + `<strong>Majority: </strong> ${vote}`;
-    })
+      .offset([-10, 0]);
     svg.call(tip);
 
     var path = d3.geo.path().projection(d3.geo.albersUsa());
@@ -64,12 +52,34 @@ export default Ember.Component.extend({
           return 'states ' + d.properties.STATE_ABBR;
         })
         .attr('d', path)
-        .on('mouseover', function(d){
+        .on('mouseover', function(d) {
+          tip.html(function(d) {
+            var state = d.properties.STATE_ABBR;
+            var vote;
+            if (!obj[state] || obj[state].d - obj[state].r === 0)
+              vote = 'Neutral';
+            else if (obj[state].d - obj[state].r > 0)
+              vote = 'Democratic';
+            else
+              vote = 'Republican';
+
+            return `State: ${state}<br/> Majority: ${vote}`;
+          });
           tip.attr('class', 'd3-tip animate').show(d);
         })
-        .on('mouseout', function(d){
+        .on('mouseout', function(d) {
           tip.attr('class', 'd3-tip').show(d)
           tip.hide();
+        })
+        .on('click', function(d) {
+          tip.hide();
+          tip.html(function(data){
+            var state = data.properties.STATE_ABBR;
+            return `State: ${state}<br/>`
+              +    `Republican: ${!obj[state] ? 0 : obj[state].r}<br/>`
+              +    `Democratic: ${!obj[state] ? 0 : obj[state].d}`;
+          });
+          tip.attr('class', 'd3-tip animate').show(d);
         });
       socket.emit('ready', {});
     });
