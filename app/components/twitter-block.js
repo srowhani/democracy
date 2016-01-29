a277e60f943c0fb6ca2ceac0368b5f5a3a0162e3
Seena Rowhani <srowhani@ciena.com>
2016-01-29 15:45:24 -0500

    todo

--- app/components/twitter-block.js
+++ app/components/twitter-block.js
@@ -2,5 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'blockquote',
-  classNames: ['twitter-tweet']
+  classNames: ['twitter-tweet'],
+  paint: Ember.observer('id', function(){
+    this.rerender();
+  })
 });
